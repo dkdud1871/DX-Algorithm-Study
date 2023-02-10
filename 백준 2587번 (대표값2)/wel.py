fe19c6fecb7a96b5e7a16156102ac91055d5d680
Sang Hun Lee <124578663+Sanghun0901@users.noreply.github.com>
2023-02-10 11:19:24 +0900

    Update wel.py

--- 백준 2587번 (대표값2)/wel.py
+++ 백준 2587번 (대표값2)/wel.py
@@ -1,1 +1,9 @@
+c = []
+for i in range(5) :
+    if len(c) < 5 :
+        c.append(int(input()))
+        
+print(int(sum(c)/len(c)))
+print(sorted(c)[2])
 
+-----------------------------------------------
