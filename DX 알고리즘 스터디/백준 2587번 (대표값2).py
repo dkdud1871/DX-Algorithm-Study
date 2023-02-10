ea85c0d0fff61d71d2b180a1843f077b7e9feadc
Sang Hun Lee <124578663+Sanghun0901@users.noreply.github.com>
2023-02-10 11:08:38 +0900

    Update 백준 2587번 (대표값2).py

--- DX 알고리즘 스터디/백준 2587번 (대표값2).py
+++ DX 알고리즘 스터디/백준 2587번 (대표값2).py
@@ -5,3 +5,15 @@ for i in range(5) :
         
 print(int(sum(c)/len(c)))
 print(sorted(c)[2])
+
+-----------------------------------------------
+
+c = []
+for i in range(5) :
+    if len(c) < 5 :
+        c.append(int(input()))
+        
+print(int(sum(c)/len(c)))
+print(sorted(c)[2])
+
+-*8
